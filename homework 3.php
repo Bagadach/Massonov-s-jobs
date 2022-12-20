139b4050de80ffed726b5906484f921004e5d7db
Bagadach <118345957+Bagadach@users.noreply.github.com>
2022-12-20 15:43:56 +0500

    Add files via upload

---
+++ homework 3.php
@@ -0,0 +1,58 @@
+<?php
+//$nameAndAges = [
+//    'Tom Hiddleston' => 41,
+//    'Arnold Schwarzenegger' => 75,
+//    'Tom Cruise' => 60,
+//    'Brad Pitt' => 58,
+//    'Tom Hanks' => 66,
+//    'Johnny Depp' => 59,
+//    'Leonardo DiCaprio' => 48,
+//    'Will Smith' => 54,
+//    'Dwayne Johnson' => 50,
+//    'Hugh Jackman' => 54,
+//    'Robert Downey Jr.' => 57,
+//    'Chris Evans' => 41,
+//    'Jackie Chan' => 68,
+//    'Jason Momoa' => 43,
+//    'Samuel L. Jackson' => 73,
+//    'Al Pacino' => 82,
+//];
+//echo "<ul><li>" . implode("</li><li>", $nameAndAges) . "</li></ul><hr>";
+//function my_sort($a,$b) {
+//    return $a <=> $b;
+//}
+//usort($nameAndAges,'my_sort');
+//echo "<ul><li>" . implode("</li><li>", $nameAndAges) . "</li></ul><hr>";
+//
+//
+//krsort($nameAndAges);
+//print_r ($nameAndAges);
+
+
+// 2 task
+//$jsonString = '{"menu": {
+//  "id": "file",
+//  "value": "File",
+//  "popup": {
+//    "menuitem": [
+//      {"value": "New", "onclick": "CreateNewDoc()"},
+//      {"value": "Open", "onclick": "OpenDoc()"},
+//      {"value": "Close", "onclick": "CloseDoc()"}
+//    ]
+//  }
+//}}';
+//$jsonArray =json_decode($jsonString);
+//echo '<pre>';
+//var_dump($jsonArray);
+//echo '</pre>';
+//$integers = [60,43,20,53,7,38,56,7,56,23,13,54,33,39,48,21,12,53,10,20,23,27,4,35,17,55,9,54,52,54,11,1,26,39,35,15,56,59,9,53,20,53,47,42,12,10,23,58,9,54];
+//$average=array_sum($integers)/count($integers);
+//echo '<pre>';
+//echo (array_sum($integers));
+//echo '</pre>';
+//echo '<pre>';
+//echo (count($integers));
+//echo '</pre>';
+//echo $average;
+
+?>
